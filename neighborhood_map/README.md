f42acf3bf5aacfafd4c2251b32266250d3b7a33f
Rui <ruizhang84@users.noreply.github.com>
2017-08-18 23:34:43 -0400

    Update Neighborhood Map App

---
+++ neighborhood_map/README.md
@@ -0,0 +1,17 @@
+# Restaurant Locations
+
+## HOW TO
+- Open index.html. The google map with restaurant markers and filter tool will be shown immediately.
+- Type in word in the provided textbox (case sensitive) on the right coner, and then click "Filter" button to filter all the listed restaurant locations by given key words.
+- To enlarge the map screen of on small device, please click the bar menu on the top of web. The Filter side tool will be hidden. 
+
+## API and libraries
+- Foursquare API
+- Google map API
+- Knockout Javascript framework
+- Bootstrap
+- Jquery
+
+## Description
+This is a udacity nanodegree course project for building up a neighborhood map app. The web provides a google map and selected markers on the top restaurants in Boston, MA. 
+The markers for restaurants provide the name, contact info, and the full address. The user can filter the locations by input some part of restaurant name to facility the searching.
---
+++ neighborhood_map/js/scripts.js
@@ -0,0 +1,133 @@
+// hiddern bar
+var viewModel = {
+  showMessage: ko.observable(true),
+  hiddenBar: function(){
+    this.showMessage(! this.showMessage());
+  },
+  restaurant: ko.observableArray(),
+  filtered: ko.observable("")
+};
+
+ko.applyBindings(viewModel);
+
+// google maps
+var map;
+var infowindow;
+const maxSearch = 11;
+
+function initMap() {
+  const Boston = {lat: 42.364506, lng: -71.038887};
+  let zoomView = 13;
+  let rad = 10000;
+  let interest = 'restaurant';
+
+  try {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: Boston,
+      zoom: zoomView
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+          location: Boston,
+          radius: rad,
+          type: interest
+        }, search);
+  }
+  catch(err){
+    alert("error!");
+    console.log(err.message);
+  }
+}
+
+function search(results, status){
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length && i < maxSearch; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+// maps marker & foursquare review
+const clientId = '1BKIHPJRF4X1IYNYTVG3KVQU01D554SFUQTO5YUQSSH1ERVL';
+const clientSecret = 'ZTPSRIOAUPU4XC1XUPJZBGHOSBC5NWBZFZCQYEUHN50IPGWI';
+const d = new Date();
+const currnetDate = d.getFullYear() +
+    ((d.getMonth()+1)<10 ? '0' : '') + (d.getMonth()+1) +
+    (d.getDate()<10 ? '0' : '') + d.getDate();
+var markers = [];
+
+function createMarker(place) {
+  let placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position:placeLoc
+  });
+  markers.push([place, marker]);
+
+  // add viewModel and listener for markers
+  viewModel.restaurant.push([place, marker]);     // !build up the restraurant list
+  google.maps.event.addListener(marker, 'click', function(){
+    markerSelect(place, marker);
+  });
+}
+
+function markerSelect (place, marker) {
+  let content = '<p class="h4 text-primary">' + place.name + '</p>';
+  // ajax to retreive info
+  $.ajax({
+    url:'https://api.foursquare.com/v2/venues/search',
+    data: {
+           ll: place.geometry.location.lat()+','+place.geometry.location.lng(), 
+           limit: 1,
+           query: place.name,
+           client_id: clientId, 
+           client_secret: clientSecret, 
+           v: currnetDate
+          },
+    dataType: "json",
+    success: function( data ) {
+      var place_data = data["response"]["venues"][0];
+      if (place_data['contact'].hasOwnProperty('formattedPhone'))
+          content += '<p>' + place_data['contact']['formattedPhone'] +'</p>';
+      if (place_data['location'].hasOwnProperty('formattedAddress'))
+          content += '<p>' + place_data['location']['formattedAddress'] + '</p>';
+      if (place_data.hasOwnProperty('url'))
+          content += '<p>' + place_data['url'];
+      if (place_data['contact'].hasOwnProperty('twitter'))
+          content += '<p> Twitter: @' + place_data['contact']['twitter'] + '</p>'
+      //console.log( JSON.stringify(place_data) );
+      infowindow.setContent(content);
+      infowindow.open(map, marker);
+    }
+  })
+  .fail(function() {
+    console.log("error");
+    // place name as only info
+    infowindow.setContent(content);
+    infowindow.open(map, marker);
+  });
+}
+
+
+// filter
+function filterMap(){
+  viewModel.restaurant.removeAll();
+  for (var i = 0; i < markers.length; i++) {
+    // filter by substring
+    let place_name = markers[i][0].name;
+    if (place_name.indexOf(viewModel.filtered()) !== -1){
+      // Sets the map on all markers in the array.
+      markers[i][1].setMap(map);
+      viewModel.restaurant.push(markers[i])
+    }else{
+      markers[i][1].setMap(null);
+    }
+  }
+}
+
+
+
+
+
